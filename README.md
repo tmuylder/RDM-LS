580b3f67ca3a94ba01c71fa8e7a758ba43dc8d4f
tmuylder <tuurmuyldermans@gmail.com>
2020-11-10 12:08:39 +0100

    Major update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RDM-LS
 Session for Research Data Management in Life Sciences workshop on 10 Nov 2020. Creating and working with a reproducible data analysis environment. 
+
+## Contributors
+- Tuur Muyldermans: [ORCID-ID](https://orcid.org/0000-0002-3926-7293)
+- Alexander Botzki: [ORCID-ID](https://orcid.org/0000-0001-6691-4233)
--- scripts/hormone.R
+++ scripts/hormone.R
@@ -1,16 +1,32 @@
-#Day 3####
-#Setting working directory ####
-folder <- "D:/trainingen/zelfgegeven/Rintro/exercises"
+## Analysis of 20201102-hormone.csv ##
+
+# Setting working directory. Change path to the folder where you installed the project. 
+folder <- "~/RDM-LS"
 setwd(folder)
 
-Hormone <- read.csv2("Rdata/Hormone.csv")
+# Import libraries
+#install.packages('ggplot2')
+library(ggplot2)
+
+
+# Import the data
+Hormone <- read.csv2("data/20201102-hormone.csv")
+# Change name of third column
+names(Hormone)[3] <- "Concentration"
 Hormone
-names(Hormone)[3] <- "conc"
 
-p2 <- ggplot(Hormone, aes(Drug,conc)) +
-	          stat_summary(fun.y=mean,geom="bar") +
-		              stat_summary(fun.data=mean_cl_normal,geom="errorbar",
-					                                           width=0.25) + geom_point() +
-  coord_flip()
+# Open a pdf file where the plot will be saved
+png(file = "results/plots/20201102-effect-drug-concentration.png", width=600, height=350) 
 
-p2
+# Create plot of effect of drug on concentration of substance. 
+Hormone_plot <- ggplot(Hormone, aes(Drug,Concentration)) +
+	          stat_summary(fun=mean,geom="bar") +
+		        stat_summary(fun.data=mean_cl_normal,geom="errorbar", width=0.25) + 
+            geom_point() +
+            coord_flip()
+
+Hormone_plot
+
+# Close the file handle
+dev.off()
+
